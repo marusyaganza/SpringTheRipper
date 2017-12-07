568d4db825812512819f0b433add10591f0fd86f
marusyaganza <marusyaganza@yandex.ru>
2017-12-07 11:29:30 +0300

    first 5 minutes

---
+++ src/main/java/quoter/TerminatorQuoter.java
@@ -0,0 +1,14 @@
+package quoter;
+
+public class TerminatorQuoter implements Quoter {
+private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void sayQuote() {
+        System.out.println(message);
+    }
+}
