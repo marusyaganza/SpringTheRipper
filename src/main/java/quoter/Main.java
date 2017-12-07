93c75022cd6f36b0f760a2d53b0203717f665864
marusyaganza <marusyaganza@yandex.ru>
2017-12-07 14:17:52 +0300

    first 46 minutes

--- src/main/java/quoter/Main.java
+++ src/main/java/quoter/Main.java
@@ -3,9 +3,15 @@ package quoter;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        context.getBean(TerminatorQuoter.class).sayQuote();
+        //Hate while(true)((:
+        int x=0;
+        while (x<100) {
+            Thread.sleep(100);
+            context.getBean(Quoter.class).sayQuote();
+            x++;
+        }
     }
 
 }
--- src/main/java/quoter/ProfilingHandlerBeanPostProcessor.java
+++ src/main/java/quoter/ProfilingHandlerBeanPostProcessor.java
@@ -4,6 +4,9 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import sun.plugin.com.BeanClass;
 
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -11,13 +14,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor {
+    private ProfillingController controller = new ProfillingController();
     private Map<String, Class> map = new HashMap<>();
+
+    public ProfilingHandlerBeanPostProcessor() throws Exception {
+        MBeanServer platformMBeanServer = ManagementFactory.getPlatformMBeanServer();
+        //It does not work!
+//        platformMBeanServer.registerMBean(controller, new ObjectName("profiling", "name", "controller"));
+    }
+
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         Class<?> beanClass = bean.getClass();
         if (beanClass.isAnnotationPresent(Profiling.class)) {
             map.put(beanName, beanClass);
         }
+
         return bean;
     }
 
@@ -28,7 +40,15 @@ public class ProfilingHandlerBeanPostProcessor implements BeanPostProcessor {
             return Proxy.newProxyInstance(beanClass.getClassLoader(), beanClass.getInterfaces(), new InvocationHandler() {
                 @Override
                 public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                    return null;
+                    if (controller.isEnabled()) {
+                        System.out.println("Profiling...");
+                        long before = System.nanoTime();
+                        Object retval = method.invoke(bean, args);
+                        long after = System.nanoTime();
+                        System.out.println(after - before);
+                        System.out.println("That's all");
+                        return retval;
+                    } else return method.invoke(bean, args);
                 }
             });
         }
